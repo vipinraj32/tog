78c7fa41809caa42ac2fa868b8b641ed63c6c312
saurabh kushwaha <saurabhkushwah438@gmail.com>
2024-11-26 20:43:45 +0530

    name and image change

--- src/container/team/Team.jsx
+++ src/container/team/Team.jsx
@@ -2,11 +2,11 @@
   import './team.css'
   import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
   import { faTwitter, faTelegramPlane, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-  import vipin from '../../assets/vipin.png'
-  import arun from '../../assets/Arun.jpg'
   import Aditya from '../../assets/aditya.jpg'
   import aniket from '../../assets/aniket.png'
   import sameer from '../../assets/sameer.jpg'
+  import saurabh from '../../assets/saurabh.jpg'
+  import shailja from '../../assets/shailja.jpg'
   const Team = () => {
     return (
       <div className='section__margin'>
@@ -42,11 +42,34 @@
             <div className="card">
               <div className="content">
                 <div className="imgBx">
-                  <img src={arun} alt="" />
+                  <img src={sameer} alt="" />
 
                 </div>
                 <div className="contentBx">
-                  <h3>Arun Pratap Singh chandel<br /><span>Co-founder</span></h3>
+                  <h3>Sameer Mishra<br /><span>Co-founder</span></h3>
+                </div>
+              </div>
+              <ul className='sci p-1'>
+                <li style={{'--i':1}}>
+                  <a href="https://x.com/sameermishra__"><FontAwesomeIcon icon={faTwitter} /></a>
+                </li><li style={{'--i':2}}>
+                  <a href="https://www.linkedin.com/in/sameer-mishra-967412239?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                </li>
+                <li style={{'--i':3}}>
+                  <a href="https://t.me/sameer_mishra612"><FontAwesomeIcon icon={faTelegramPlane} /></a>
+                </li>
+
+              </ul>
+            </div>
+            
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src={shailja} alt="" />
+
+                </div>
+                <div className="contentBx">
+                  <h3>Shailja Soni <br /><span>Full Stack developer</span></h3>
                 </div>
               </div>
               <ul className='sci p-1'>
@@ -61,49 +84,26 @@
 
               </ul>
             </div>
-            
-            <div className="card">
-              <div className="content">
-                <div className="imgBx">
-                  <img src={vipin} alt="" />
-
-                </div>
-                <div className="contentBx">
-                  <h3>Vipin Singh <br /><span>Full Stack developer</span></h3>
-                </div>
-              </div>
-              <ul className='sci p-1'>
-                <li style={{'--i':1}}>
-                  <a href="https://x.com/FlexDev_Vipin"><FontAwesomeIcon icon={faTwitter} /></a>
-                </li><li style={{'--i':2}}>
-                  <a href="https://www.linkedin.com/in/vipin-rajpoot-2b019b261/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-                </li>
-                <li style={{'--i':3}}>
-                  <a href="https://t.me/vipinsingh2005"><FontAwesomeIcon icon={faTelegramPlane} /></a>
-                </li>
-
-              </ul>
-            </div>
 
             
             <div className="card">
               <div className="content">
                 <div className="imgBx">
-                  <img src={sameer} alt="" />
+                  <img src={saurabh} alt="" />
 
                 </div>
                 <div className="contentBx">
-                  <h3>Sameer Mishra<br /><span>community manager</span></h3>
+                  <h3>Saurabh Kushwaha<br /><span>community manager</span></h3>
                 </div>
               </div>
               <ul className='sci p-1'>
                 <li style={{'--i':1}}>
-                  <a href="https://x.com/sameermishra__"><FontAwesomeIcon icon={faTwitter} /></a>
+                  <a href=""><FontAwesomeIcon icon={faTwitter} /></a>
                 </li><li style={{'--i':2}}>
-                  <a href="https://www.linkedin.com/in/sameer-mishra-967412239?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                  <a href="https://www.linkedin.com/in/saurabhkushwaha438/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
                 </li>
                 <li style={{'--i':3}}>
-                  <a href="https://t.me/sameer_mishra612"><FontAwesomeIcon icon={faTelegramPlane} /></a>
+                  <a href=""><FontAwesomeIcon icon={faTelegramPlane} /></a>
                 </li>
 
               </ul>
